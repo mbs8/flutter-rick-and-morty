6533ecffbb8845ed655567f9ab1342985794f98c
Matheus Branco <matheusbsiqueira96@gmail.com>
2023-12-06 09:22:18 -0300

    add settings page with external links

---
+++ rick_and_morty/lib/settings/presentation/page/settings_page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:rick_and_morty/settings/presentation/widgets/settings_widget.dart';
+
+class SettingsPage extends StatelessWidget {
+  const SettingsPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const SettingsWidget();
+  }
+}
