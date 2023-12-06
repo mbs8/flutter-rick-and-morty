ca16418117a981748f3b817f73ab3077b6fdc792
Matheus Branco <matheusbsiqueira96@gmail.com>
2023-12-06 09:42:26 -0300

    creates basics light and dark themes

---
+++ rick_and_morty/lib/shared/theme/data/app_theme_model.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'app_theme.dart';
+
+class AppThemeModel {
+  AppThemeModel({
+    required this.theme,
+    required this.themeData,
+  });
+
+  final AppTheme theme;
+  final ThemeData themeData;
+}
