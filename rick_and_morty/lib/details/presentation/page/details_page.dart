a3a268dd00f1a593908877b195ccfc7d1b451985
Matheus Branco <matheusbsiqueira96@gmail.com>
2023-12-07 10:07:05 -0300

    finish details page UI, use custom pair datastructure - class 18

--- rick_and_morty/lib/details/presentation/page/details_page.dart
+++ rick_and_morty/lib/details/presentation/page/details_page.dart
@@ -17,8 +17,10 @@ class DetailsPage extends StatelessWidget {
     return BlocProvider(
       create: (_) => GetIt.I.get<CharacterDetailsBloc>()
         ..add(CharacterDetailsRequestEvent(characterId)),
-      child: DetailsWidget(
-        characterId: characterId,
+      child: Scaffold(
+        body: DetailsWidget(
+          characterId: characterId,
+        ),
       ),
     );
   }
