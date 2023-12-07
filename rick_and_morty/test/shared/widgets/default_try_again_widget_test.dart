26e67eb8e511a723c83049525be3f30d6d176078
Matheus Branco <matheusbsiqueira96@gmail.com>
2023-12-07 16:55:34 -0300

    delete default flutter widget test, implements custom widgets tests

---
+++ rick_and_morty/test/shared/widgets/default_try_again_widget_test.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:rick_and_morty/shared/widgets/default_try_again_widget.dart';
+
+void main() {
+  group('circle widget default tests', () {
+    testWidgets('has only one try again message', (tester) async {
+      await tester.pumpWidget(
+        const MaterialApp(
+          home: DefaultTryAgainWidget(),
+        ),
+      );
+
+      expect(find.text('Try Again'), findsOneWidget);
+    });
+    testWidgets('has only one something wrong message', (tester) async {
+      await tester.pumpWidget(
+        const MaterialApp(
+          home: DefaultTryAgainWidget(),
+        ),
+      );
+
+      expect(find.text('Ops, something wrong happened'), findsOneWidget);
+      expect(find.text(';)'), findsNothing);
+    });
+  });
+}
